464d7e3677d989bbc4ae99aea0698287afcb48eb
Roman Skrylnyk <romam65@outlook.com>
2022-07-21 18:32:01 +0300

    Updated Chrome rev0.2

--- my_initial.sh
+++ my_initial.sh
@@ -6,15 +6,17 @@ apt install gparted -y
 apt install remmina -y
 apt install libreoffice -y
 apt install ssh -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
 snap install telegram-desktop
 apt install gnome-tweak-tool -y
 apt install xrdp -y
 add-apt-repository ppa:videolan/master-daily
 apt update
-apt-get install vlc qtwayland5
+apt-get install vlc qtwayland5 -y
 add-apt-repository ppa:webupd8team/atom
 apt-get update
-apt-get install atom
+apt-get install atom -y
 apt-get install gimp
 add-apt-repository ppa:deluge-team / ppa
 apt-get update
