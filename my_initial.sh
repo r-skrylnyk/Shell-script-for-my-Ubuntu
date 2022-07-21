73116ef98e5bf5365de35f50fb745907ef18ec62
Roman Skrylnyk <romam65@outlook.com>
2022-07-21 17:59:23 +0300

    Added sh script rev0.1

---
+++ my_initial.sh
@@ -0,0 +1,23 @@
+#My preset use Ubuntu for computer.
+!/bin/bash
+apt update && apt upgrade -y
+apt install net-tools -y
+apt install gparted -y
+apt install remmina -y
+apt install libreoffice -y
+apt install ssh -y
+snap install telegram-desktop
+apt install gnome-tweak-tool -y
+apt install xrdp -y
+add-apt-repository ppa:videolan/master-daily
+apt update
+apt-get install vlc qtwayland5
+add-apt-repository ppa:webupd8team/atom
+apt-get update
+apt-get install atom
+apt-get install gimp
+add-apt-repository ppa:deluge-team / ppa
+apt-get update
+apt-get install deluge
+#----------------------------------------------------------------------------------------
+#Roman Skrylnyk, rev0.1.
