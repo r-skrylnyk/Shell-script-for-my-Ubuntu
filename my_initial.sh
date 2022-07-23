7817e9fb88f32b266b62080d25516d50c31699e5
Roman Skrylnyk <romam65@outlook.com>
2022-07-23 14:40:33 +0300

    Update initial.sh. Added Git, Ansible, vlc.

--- my_initial.sh
+++ my_initial.sh
@@ -2,6 +2,7 @@
 !/bin/bash
 apt update && apt upgrade -y
 apt install net-tools -y
+apt install git -y
 apt install gparted -y
 apt install remmina -y
 apt install libreoffice -y
@@ -17,9 +18,14 @@ apt-get install vlc qtwayland5 -y
 add-apt-repository ppa:webupd8team/atom
 apt-get update
 apt-get install atom -y
+#Why not?
+apt install -y software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt update
+apt install -y ansible
 apt-get install gimp
 add-apt-repository ppa:deluge-team / ppa
 apt-get update
 apt-get install deluge
 #----------------------------------------------------------------------------------------
-#Roman Skrylnyk, rev0.1.
+#Roman Skrylnyk, rev0.3.
